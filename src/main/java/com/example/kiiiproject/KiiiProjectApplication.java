64a920f63dd967c5287ba8d5831e97e8deca0175
keremkubur <kerem.kubur@gmail.com>
2023-01-22 12:13:14 +0100

    initial

---
+++ src/main/java/com/example/kiiiproject/KiiiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiiiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiiiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiiiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kiiiproject/KiiiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiiiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiiiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
